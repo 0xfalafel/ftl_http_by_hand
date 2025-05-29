b8a7d9c95ee46d1c1e3168044aeda56a47bd7dfe
Olivier Lasne <lasneolivier@gmail.com>
2025-05-29 20:19:10 +0200

    created certificate store

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{str::FromStr, sync::Arc};
 use color_eyre::eyre::eyre;
 use nom::Offset;
 use rustls::{ClientConfig, KeyLogFile, RootCertStore};
+use rustls::pki_types::CertificateDer;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
@@ -13,6 +14,22 @@ use tracing_subscriber::{filter::targets::Targets, layer::SubscriberExt, util::S
 #[tokio::main]
 async fn main() -> color_eyre::Result<()>{
     color_eyre::install().unwrap();
+
+    let filter_layer = Targets::from_str(std::env::var("RUST_LOG").as_deref().unwrap_or("info")).unwrap();
+    let format_layer = tracing_subscriber::fmt::layer();
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(format_layer)
+        .init();
+
+    info!("Establishing TCPconnection...");
+    let stream = TcpStream::connect("example.com:443").await?;
+
+    info!("Setting up TLS root certificate store");
+    let mut roots = rustls::RootCertStore::empty();
+    for cert in rustls_native_certs::load_native_certs().expect("could not load platform certs") {
+        roots.add(cert).unwrap();
+    }
 
     
     println!("Hello, world!");
