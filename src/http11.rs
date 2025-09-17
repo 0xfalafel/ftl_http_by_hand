56ca4a3aa201d309510ed4e472aaf804e5f695a1
Olivier Lasne <lasneolivier@gmail.com>
2025-09-17 11:34:55 +0200

    parse HTTP headers

--- src/http11.rs
+++ src/http11.rs
@@ -1,5 +1,5 @@
 use nom::{
-    bytes::streaming::{tag, take_until, take_while1}, character::complete::{space1, u16}, combinator::map_res, IResult, Parser
+    bytes::streaming::{tag, take_until, take_while1}, character::complete::{space0, space1, u16}, combinator::{map_res, opt}, sequence::terminated, IResult, Parser
 };
 
 #[derive(Debug)]
@@ -16,20 +16,54 @@ const CRLF: &str = "\r\n";
 
 // Looks like `HTTP/1.1 200 OK\r\n` or `HTTP/1.1 404 Not Found\r\n`
 pub fn response(i: &[u8]) -> IResult<&[u8], Response<'_>> {
+    // HTTP version header
     let (i, _) = tag("HTTP/1.1")(i)?;
     let (i, _) = space1(i)?;
     
+    // Read the status code
     let (i, status_code) = u16(i)?;
     let (i, _) = space1(i)?;
-    println!("status_code: {}", status_code);
     
+    // Read the status text
     let (i, status_text) = map_res(
         take_until(CRLF),
         |bytes| str::from_utf8(bytes)
     ).parse(i)?;
-    println!("status_text: {}", status_text);
-
     let (i, _) = tag(CRLF)(i)?;
+    
+    let mut res = Response {
+        status: status_code,
+        status_text: status_text,
+        headers: Vec::default(),
+    };
 
-    todo!();
+    let mut i = i;
+    loop {
+        if let (i, Some(_)) = opt(tag(CRLF)).parse(i)? {
+            println!("We are done !");
+            return Ok((i, res))
+        }
+
+        let (i2, (name, value)) = header(i)?;
+        res.headers.push((name, value));
+        i = i2;
+    }
+}
+
+fn header(i: &[u8]) -> IResult<&[u8], (&str, &str)> {
+    // parse the header key
+    let (i, name) = map_res(
+        terminated(take_until(":"), tag(":")),
+        str::from_utf8
+    ).parse(i)?;
+
+    let (i, _) = space0(i)?;
+
+    // parse the header value
+    let (i, value) = map_res(
+        terminated(take_until(CRLF), tag(CRLF)),
+        str::from_utf8
+    ).parse(i)?;
+
+    Ok((i, (name, value)))
 }
